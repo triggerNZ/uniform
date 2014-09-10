c9dc023068b4cb6db7b9afa8f912c1af8a55741a
Stephan Hoermann <srhoermann@gmail.com>
2014-09-10 11:32:56 +1000

    Adds lint:unchecked javac option to ScalaSettings.

--- uniform-core/src/main/scala/au/com/cba/omnia/uniform/core/setting/ScalaSettings.scala
+++ uniform-core/src/main/scala/au/com/cba/omnia/uniform/core/setting/ScalaSettings.scala
@@ -33,6 +33,7 @@ object ScalaSettings extends Plugin {
         "-target:jvm-1.6"
       ),
       javacOptions ++= Seq(
+        "-Xlint:unchecked",
         "-source", "1.6",
         "-target", "1.6"
       )
